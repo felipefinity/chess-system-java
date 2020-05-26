6cd4127467a4fbc0f2ab741296001d51ad894509
felipefinity <felipe-augusto1997@hotmail.com>
2020-05-26 17:11:48 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,9 @@ import chess.Color;
     	System.out.println();
     	System.out.println("Turn : " + chessMatch.getTurn());
     	System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    	if (chessMatch.getCheck()) {
+    		System.out.println("CHECK!");
+    	}
     }
 	
 	public static void printBoard(ChessPiece[][] pieces) {
